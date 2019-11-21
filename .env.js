05016780d3391b3c99bdd34709cd455a792a1cfb
Daniel Massarsky <alj@qlik.com>
2019-11-21 16:04:28 -0500

    added env config, webpack update

---
+++ .env.js
@@ -0,0 +1,11 @@
+module.exports = {
+    development: {
+        build_output: 'C:/QlikShare/StaticContent/Extensions/superNav',
+        port: 8099,
+        ext_name: 'superNav'
+    },
+    production: {
+        build_output: 'build',
+        ext_name: 'superNav'
+    }
+}
